6fadb2a9f31836e5626b1ab53b8e63191f91609d
Heitor Chang <heitorchang@gmail.com>
2019-05-06 09:00:13 -0300

    add five minutes to time

--- new_todo.php
+++ new_todo.php
@@ -10,7 +10,7 @@ require("header.php");
 <input name="todolist_id" type="hidden" value="<?= $_GET['id'] ?>">
 
 Name: <input name="name" size="70" autofocus><br>
-Date: <input name="date" type="date" value="<?= date('Y-m-d', time()) ?>"><br>
+Date: <input name="date" type="date" value="<?= date('Y-m-d', time() + 300) ?>"><br>
 Time: <input name="time" type="time" value="<?= date('H:i', time() + 300) ?>"><br>
 <br>
 <input type="submit">
