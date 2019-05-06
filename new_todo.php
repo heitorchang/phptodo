432821168def935102d8013a8dc8e8e6570e29fe
Heitor Chang <heitorchang@gmail.com>
2019-05-06 09:02:08 -0300

    use one hour

--- new_todo.php
+++ new_todo.php
@@ -10,8 +10,8 @@ require("header.php");
 <input name="todolist_id" type="hidden" value="<?= $_GET['id'] ?>">
 
 Name: <input name="name" size="70" autofocus><br>
-Date: <input name="date" type="date" value="<?= date('Y-m-d', time() + 300) ?>"><br>
-Time: <input name="time" type="time" value="<?= date('H:i', time() + 300) ?>"><br>
+Date: <input name="date" type="date" value="<?= date('Y-m-d', time() + 3600) ?>"><br>
+Time: <input name="time" type="time" value="<?= date('H:i', time() + 3600) ?>"><br>
 <br>
 <input type="submit">
 </form>
