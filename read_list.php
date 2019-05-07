81cccab3a565f36a3faa1aac9a3c5c4ffee6e61b
Heitor Chang <heitorchang@gmail.com>
2019-05-07 17:02:59 -0300

    put edit del in js file

--- read_list.php
+++ read_list.php
@@ -163,17 +163,4 @@ foreach ($stmt as $row) {
 
 </table>
 
-<script>
-function togglelink(id) {
-  var boxstate = document.getElementById("checkbox_" + id).checked;
-  var el = document.getElementById("edit_" + id);
-  var dl = document.getElementById("delete_" + id);
-  if (boxstate) {
-     el.style = "display: none;";
-     dl.style = "display: block;";
-  } else {
-     dl.style = "display: none;";
-     el.style = "display: block;";
-  }  
-}
-</script>
+<script src="editdel.js"></script>
